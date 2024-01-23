07fa89a82df0d9ec3db0ab41fd0886f9c79c5fa4
chaewon <cwlee4633@naver.com>
2024-01-23 17:00:21 +0900

    git init

---
+++ src/test/kotlin/com/example/kotlinprac/KotlinPracApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinprac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPracApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
